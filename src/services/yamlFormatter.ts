44a8ec0382fb7b8ed99c010eb9a736dd1b922711
Adam Voss <vossad01@gmail.com>
2017-06-25 17:20:19 -0500

    Add formatting support for multiple documents

--- src/services/yamlFormatter.ts
+++ src/services/yamlFormatter.ts
@@ -1,13 +1,24 @@
 'use strict';
 
 import jsyaml = require('js-yaml')
+import {EOL} from 'os'
 import { TextDocument, Range, Position, FormattingOptions, TextEdit } from 'vscode-languageserver-types';
 
 export function format(document: TextDocument, options: FormattingOptions): TextEdit[] {
     const text = document.getText()
 
-    const yaml = jsyaml.load(text)
-    const newText = jsyaml.safeDump(yaml, { indent: options.tabSize })
+    const documents = []
+    jsyaml.loadAll(text, doc => documents.push(doc))
+
+    let newText;
+    if (documents.length == 1) {
+        const yaml = documents[0]
+        newText = jsyaml.safeDump(yaml, { indent: options.tabSize })
+    }
+    else{
+        const formatted = documents.map(d => jsyaml.safeDump(d, { indent: options.tabSize }))
+        newText = '%YAML 1.2' + EOL + '---' + EOL + formatted.join('...' + EOL + '---' + EOL) + '...' + EOL
+    }
 
     return [TextEdit.replace(Range.create(Position.create(0, 0), document.positionAt(text.length)), newText)]
 }
--- src/test/yamlFormatter.test.ts
+++ src/test/yamlFormatter.test.ts
@@ -1,5 +1,5 @@
 import { format } from '../services/yamlFormatter'
-import { TextDocument /*, Range, Position, FormattingOptions, TextEdit */ } from 'vscode-languageserver-types';
+import { TextDocument } from 'vscode-languageserver-types';
 import assert = require("assert");
 
 suite("YAML Formatter", () => {
@@ -32,6 +32,71 @@ suite("YAML Formatter", () => {
   warehouseLocation:
     latitude: -78.75
     longitude: 20.4
+`)
+  })
+
+  test('multiple documents', function () {
+    assertFormatedEqual(`%YAML 1.2
+---
+- id: 2
+  name: An ice sculpture
+  price: 12.5
+  tags:
+    - cold
+    - ice
+  dimensions:
+    length: 7
+    width: 12
+    height: 9.5
+  warehouseLocation:
+    latitude: -78.75
+    longitude: 20.4
+...
+---
+[
+  {
+    "id": 3,
+    "name": "A blue mouse",
+    "price": 25.5,
+    "dimensions": {
+      "length": 3.1,
+      "width": 1,
+      "height": 1
+    },
+    "warehouseLocation": {
+      "latitude": 54.4,
+      "longitude": -32.7
+    }
+  }
+]
+...`, `%YAML 1.2
+---
+- id: 2
+  name: An ice sculpture
+  price: 12.5
+  tags:
+    - cold
+    - ice
+  dimensions:
+    length: 7
+    width: 12
+    height: 9.5
+  warehouseLocation:
+    latitude: -78.75
+    longitude: 20.4
+...
+---
+- id: 3
+  name: A blue mouse
+  price: 25.5
+  dimensions:
+    length: 3.1
+    width: 1
+    height: 1
+  warehouseLocation:
+    latitude: 54.4
+    longitude: -32.7
+...
 `)
   })
 })
